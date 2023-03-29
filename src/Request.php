dfc6896077640dd4dd71e103f6f320c3db9ac0b9
MartaMarija <mpicic@student.foi.hr>
2023-03-29 15:19:25 +0200

    Request class and interface
    Request obj can retrieve headers, body, params and their specific values;

--- src/Request.php
+++ src/Request.php
@@ -2,7 +2,60 @@
 
 namespace App;
 
-class Request
-{
+use Couchbase\BadInputException;
 
+class Request implements RequestInterface
+{
+    private array $headers;
+    private array $params;
+    private array $body;
+    
+    public function __construct(array $headers, array $params, array $body)
+    {
+        $this->headers = $headers;
+        $this->params = $params;
+        $this->body = $body;
+    }
+    
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+    
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+    
+    public function getBody(): array
+    {
+        return $this->body;
+    }
+    
+    function getHeadersValue(string $key): string
+    {
+        if (!array_key_exists($key, $this->headers)) {
+            //TODO throw Error
+            return "Ne postoji";
+        }
+        return $this->headers[$key];
+    }
+    
+    function getBodyValue(string $key): string
+    {
+        if (!array_key_exists($key, $this->body)) {
+            //TODO throw Error
+            return "Ne postoji";
+        }
+        return $this->body[$key];
+    }
+    
+    function getParamsValue(string $key): string
+    {
+        if (!array_key_exists($key, $this->params)) {
+            //TODO throw Error
+            return "Ne postoji";
+        }
+        return $this->params[$key];
+    }
 }
--- src/RequestInterface.php
+++ src/RequestInterface.php
@@ -4,5 +4,15 @@ namespace App;
 
 interface RequestInterface
 {
-
+    function getHeaders(): array;
+    
+    function getParams(): array;
+    
+    function getBody(): array;
+    
+    function getHeadersValue(string $key): string;
+    
+    function getBodyValue(string $key): string;
+    
+    function getParamsValue(string $key): string;
 }
