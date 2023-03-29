9364085d8ac7df056159d4aa1b4a203b7d88a37d
MartaMarija <mpicic@student.foi.hr>
2023-03-29 16:08:59 +0200

    Router.php and routes.php - have to add Response

--- src/Router.php
+++ src/Router.php
@@ -4,7 +4,32 @@ namespace App;
 
 class Router
 {
-    public function addRoute($url, $method, $callback){
-
+    private array $routes;
+    private static mixed $router;
+    
+    public static function getRouter()
+    {
+        $cls = static::class;
+        if (!isset(self::$router[$cls])) {
+            self::$router[$cls] = new static();
+        }
+        return self::$router[$cls];
+    }
+    
+    public function addRoute($method, $url, $callback): void
+    {
+        $route_data = array("method" => $method, "url" => $url, "callback" => $callback);
+        $this->routes[] = $route_data;
+    }
+    
+    public function resolveRoute($method, $url, RequestInterface $request)
+    {
+        foreach ($this->routes as $route) {
+            if ($route["method"] === $method && $route["url"] === $url) {
+                return call_user_func($route["callback"], $request);
+            }
+        }
+        return "Ruta: '" . $url . "' + Metoda: '" . $method . "' ne postoji!";
     }
 }
+
--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,18 @@
 <?php
 
+use App\Router;
+use App\RequestInterface;
+
+
+$router = Router::getRouter();
+
+$router->addRoute("GET", "/index.php", function (RequestInterface $request) {
+    return "POST methoda: ime => " . $request->getParamsValue("ime");
+});
+
+$router->addRoute("POST", "/index.php", function (RequestInterface $request) {
+    return "POST methoda: ime => " . $request->getParamsValue("ime");
+});
+
+
 
