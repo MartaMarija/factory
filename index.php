fe350f21e8360429efa9af5385fbf06e0a627523
MartaMarija <mpicic@student.foi.hr>
2023-03-29 16:09:45 +0200

    index.php - have to add Response

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 
 require_once 'vendor/autoload.php';
 
+use App\Request;
+use App\Router;
+
+include("./src/routes.php");
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method != "POST" && $method != "GET") {
+    echo "Metoda '" . $method . "' nije podržana";
+    return;
+}
+
+
+$request = new Request(apache_request_headers(), $_GET, $_POST);
+
+$router = Router::getRouter();
+$response = $router->resolveRoute($method, $url, $request);
+echo $response;
+
+
+
 
